5fbb27d4c5cbe0dbd5e661b991f7a640abcd4590
Anderson Cruz <ac.andersoncruz@gmail.com>
2019-05-29 20:21:22 -0400

    Ajustando para video

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # import the necessary packages
 import numpy as np
 import cv2
+import os
 
-# python deep_learning_object_detection.py \
-# 	--prototxt MobileNetSSD_deploy.prototxt.txt \
-# 	--model MobileNetSSD_deploy.caffemodel --image images/example_01.jpg
 
-image_path = "image_1.jpg"
+VIDEO_INPUT = "video.mp4"
+
 prototxt = "models/deploy.prototxt.txt"
 model = "models/MobileNetSSD_deploy.caffemodel"
 CONST_CONFIDENCE = 0.1
@@ -14,10 +13,13 @@ CONST_CONFIDENCE = 0.1
 # initialize the list of class labels MobileNet SSD was trained to
 # detect, then generate a set of bounding box colors for each class
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-	"sofa", "train", "tvmonitor"]
+    "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+    "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+    "sofa", "train", "tvmonitor"]
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# force color to car be red
+COLORS[7] = (0, 0, 255)
 
 # load our serialized model from disk
 print("[INFO] loading model...")
@@ -27,47 +29,80 @@ net = cv2.dnn.readNetFromCaffe(prototxt, model)
 # by resizing to a fixed 300x300 pixels and then normalizing it
 # (note: normalization is done via the authors of the MobileNet SSD
 # implementation)
-image = cv2.imread(image_path)
-(h, w) = image.shape[:2]
-blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843,
-	(300, 300), 127.5)
 
-# pass the blob through the network and obtain the detections and
-# predictions
-print("[INFO] computing object detections...")
-net.setInput(blob)
-detections = net.forward()
+video = cv2.VideoCapture(os.path.join("input", VIDEO_INPUT))
 
-# pass the blob through the network and obtain the detections and
-# predictions
-print("[INFO] computing object detections...")
-net.setInput(blob)
-detections = net.forward()
+j = 1
 
-# loop over the detections
-for i in np.arange(0, detections.shape[2]):
-	# extract the confidence (i.e., probability) associated with the
-	# prediction
-	confidence = detections[0, 0, i, 2]
+while(video.isOpened()):
+    print("j: ", j)
+    ret, frame = video.read()
 
-	# filter out weak detections by ensuring the `confidence` is
-	# greater than the minimum confidence
-	if confidence > CONST_CONFIDENCE:
-		# extract the index of the class label from the `detections`,
-		# then compute the (x, y)-coordinates of the bounding box for
-		# the object
-		idx = int(detections[0, 0, i, 1])
-		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-		(startX, startY, endX, endY) = box.astype("int")
+    if ret == False:
+        break
 
-		# display the prediction
-		label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
-		print("[INFO] {}".format(label))
-		cv2.rectangle(image, (startX, startY), (endX, endY),
-			COLORS[idx], 2)
-		y = startY - 15 if startY - 15 > 15 else startY + 15
-		cv2.putText(image, label, (startX, y),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+    print("ret: ", ret)
+    (h, w) = frame.shape[:2]
 
-# show the output image
-cv2.imwrite("output.png", image)
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.014843,
+        (300, 300), 0.0) #1050
+
+
+
+    # pass the blob through the network and obtain the detections and
+    # predictions
+    print("[INFO] computing object detections...")
+    net.setInput(blob)
+    detections = net.forward()
+
+    flag = False
+
+    # loop over the detections
+    for i in np.arange(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with the
+        # prediction
+        confidence = detections[0, 0, i, 2]
+
+
+
+        # filter out weak detections by ensuring the `confidence` is
+        # greater than the minimum confidence
+        if confidence > CONST_CONFIDENCE:
+            # extract the index of the class label from the `detections`,
+            # then compute the (x, y)-coordinates of the bounding box for
+            # the object
+            idx = int(detections[0, 0, i, 1])
+
+            if CLASSES[idx] == "car":
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+
+                # display the prediction
+                label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+                print("[INFO] {}".format(label))
+                cv2.rectangle(frame, (startX, startY), (endX, endY),
+                    COLORS[idx], 2)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(frame, label, (startX, y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+                flag = True
+    if flag:
+        # show the output image
+        cv2.imwrite(os.path.join("output", VIDEO_INPUT.split(".")[0] + "_" + str(j) + "_out.png"), frame)
+    j += 1
+
+video.release()
+
+# cap = cv2.VideoCapture('vtest.avi')
+#
+# while(cap.isOpened()):
+#     ret, frame = cap.read()
+#
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#
+#     cv2.imshow('frame',gray)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+#
+# cap.release()
+# cv2.destroyAllWindows()
